16e9dec7712143104de364f78645d91332c0ac4c
KetaoChen <kchen343@gatech.edu>
2020-02-22 03:44:31 -0500

    ngif login

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -8,13 +8,18 @@ import { Component, OnInit } from "@angular/core";
 export class LoginComponent implements OnInit {
   username = "Charlesna";
   password = "31415926";
+  errorMessage = "Invalid Credentials";
+  invalidLogin = false;
 
   constructor() {}
 
   ngOnInit() {}
 
   handleLogin() {
-    console.log(this.username);
-    console.log(this.password);
+    if (this.username === "Charlesna" && this.password === "31415926") {
+      this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+    }
   }
 }

483beb7185b8c4df5d99e4c5820f5c13e96f72ca
KetaoChen <kchen343@gatech.edu>
2020-02-23 21:01:38 -0500

    ng HttpClient module

--- frontend/todo/src/app/app.module.ts
+++ frontend/todo/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
 import { LoginComponent } from "./login/login.component";
-import { ErrorComponent } from './error/error.component';
-import { ListToDosComponent } from './list-to-dos/list-to-dos.component';
-import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
-import { LogoutComponent } from './logout/logout.component';
+import { ErrorComponent } from "./error/error.component";
+import { ListToDosComponent } from "./list-to-dos/list-to-dos.component";
+import { MenuComponent } from "./menu/menu.component";
+import { FooterComponent } from "./footer/footer.component";
+import { LogoutComponent } from "./logout/logout.component";
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, LoginComponent, ErrorComponent, ListToDosComponent, MenuComponent, FooterComponent, LogoutComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    ErrorComponent,
+    ListToDosComponent,
+    MenuComponent,
+    FooterComponent,
+    LogoutComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/todo/src/app/service/data/welcome-data.service.ts
+++ frontend/todo/src/app/service/data/welcome-data.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
 })
 export class WelcomeDataService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   executeHelloWorldBeanService() {
-    console.log("Execute Hello World Bean Service");
+    console.log(this.http.get("http://localhost:8080/hello-world"));
   }
 }

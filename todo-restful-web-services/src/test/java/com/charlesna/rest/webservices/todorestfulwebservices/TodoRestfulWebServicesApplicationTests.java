8bc226623110c667df9f4245dc68ad3afee22d74
KetaoChen <kchen343@gatech.edu>
2020-02-23 20:00:50 -0500

    initial spring

---
+++ todo-restful-web-services/src/test/java/com/charlesna/rest/webservices/todorestfulwebservices/TodoRestfulWebServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.charlesna.rest.webservices.todorestfulwebservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoRestfulWebServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

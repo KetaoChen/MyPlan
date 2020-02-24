37bb9275ec9fcfaffd4286f1cc8b277cecd47edd
KetaoChen <kchen343@gatech.edu>
2020-02-23 20:17:30 -0500

    hello world bean

--- todo-restful-web-services/src/main/java/com/charlesna/rest/webservices/todorestfulwebservices/HelloWorldController.java
+++ todo-restful-web-services/src/main/java/com/charlesna/rest/webservices/todorestfulwebservices/HelloWorldController.java
@@ -1,8 +1,6 @@
 package com.charlesna.rest.webservices.todorestfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
@@ -12,6 +10,12 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    //hello-world-bean
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 
 }
